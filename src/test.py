8f151c3df213b62aca4ee01e8a6659970cef883e
ariyn <himnowxz@gmail.com>
2018-03-10 13:54:45 +0900

    jenkins test #2

--- src/test.py
+++ src/test.py
@@ -1,1 +1,10 @@
-print("HI!")
+"""
+written ariyn(himnowxz@gmail.com)
+this is some sample python script for jenkins practice
+"""
+
+def testFunc():
+    """
+    this is sample function
+    """
+    print("HI!")
